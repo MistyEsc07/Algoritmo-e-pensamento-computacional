7b067ac8e3c46fb03bf3142d1973da372575deda
Misty <mariaeduardaescobarciprianobr@gmail.com>
2026-09-20 20:17:12 -0300

    Add comments for variable declarations and logic
    Added comments to clarify variable usage and program flow.

--- desafio-monitoramento/monitoramento.c
+++ desafio-monitoramento/monitoramento.c
@@ -2,22 +2,22 @@
 
 int main() {
     float limite, temperatura;
-    float soma = 0;
+    float soma = 0;              (Aqui são as variáveis declaradas e utilizadas nas operações)
     float media, maior, menor;
 
-    int quantidade, i;
-    int acimaLimite = 0;
+    int quantidade, i;        (Variáveis que serão limitadas pelo usuário.)
+    int acilimite = 0;
 
-    do {printf("Digite o limite de temperatura: ");
+    do {printf("Digite o limite de temperatura: ");   (Primeira pergunta.)
         scanf("%f", &limite);
 
         if (limite < 0) {
-            printf("Valor invalido! Digite um valor maior ou igual a 0.\n");
+            printf("Valor invalido! Digite um valor maior ou igual a 0.\n");   (Caso a pessoa coloque uma letra ou qualquer string que não corresponde com decimal.)
         }
 
     } while (limite < 0);
 
-    printf("Digite quantas temperaturas serão inseridas. ");
+    printf("Digite quantas temperaturas serão inseridas. ");    (Quantidades de temperaturas que o usuário vai colocar, podendo ser 2, 3 ou quantas forem tragas.)
     scanf("%d", &quantidade);
 
     for (i = 0; i < quantidade; i++) {
@@ -32,22 +32,22 @@ int main() {
 
         soma = soma + temperatura;
 
-        if (temperatura > maior) {
+        if (temperatura > maior) {   (Verificação da maior temperatura)
             maior = temperatura;
         }
 
-        if (temperatura < menor) {
+        if (temperatura < menor) {    (Verificação da menor temperatura)
             menor = temperatura;
         }
 
-        if (temperatura > limite) {
+        if (temperatura > limite) {   (Comparação com o limite de temperatura declarado)
             acimaLimite++;
         }
     }
 
     media = soma / quantidade;
 
-    printf("Limite de temperatura: %.2f\n", limite);
+    printf("Limite de temperatura: %.2f\n", limite);            (Esse é o relatório que é feito depois das perguntas e operação realizada.)
     printf("Quantidade de temperaturas: %d\n", quantidade);
     printf("Media: %.2f\n", media);
     printf("Maior temperatura: %.2f\n", maior);
